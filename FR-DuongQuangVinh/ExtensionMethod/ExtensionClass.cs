9fbd27058dbc78b656470c0337271a6865405f7b
vinhdqph17240 <vinhdqph17240@fpt.edu.vn>
2022-08-17 15:16:21 +0700

    .

---
+++ FR-DuongQuangVinh/ExtensionMethod/ExtensionClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class ExtensionClass
+    {
+        public static List<People> WhereExtension(this List<People> peoples, string s)
+        {
+            List<People> lstpeoples = new List<People>();
+            foreach (var x in peoples)
+            {
+                if (x.Sex == s || x.Name == s || x.NumberPhone == s)
+                {
+                    lstpeoples.Add(x);
+                }
+            }
+            return lstpeoples;
+        }
+    }
+}
+
---
+++ FR-DuongQuangVinh/ExtensionMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // linq where binh thuong
+            string s;
+            People people = new People();
+            foreach (People pp in people._lstpeoples.Where(c => c.Sex == "Nu"))
+            {
+                pp.printToTheScreen();
+            }
+            Console.WriteLine("---------------------------------");
+            //ExtensionMethod
+            Console.WriteLine("Nhap chuoi de tim kiem");
+            s = Console.ReadLine();
+            foreach (People pp in people._lstpeoples.WhereExtension(s))
+            {
+                pp.printToTheScreen();
+            }
+            
+
+        }
+    }
+}
